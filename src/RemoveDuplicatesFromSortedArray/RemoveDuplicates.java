7417265a8f8249197ef2da3dea915cc9ec2a8db0
Yusuf Kaya <karalortgt@gmail.com>
2025-12-10 12:36:00 +0200

    Add initial coding challenges structure

---
+++ src/RemoveDuplicatesFromSortedArray/RemoveDuplicates.java
@@ -0,0 +1,16 @@
+package RemoveDuplicatesFromSortedArray;
+
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int index = 1;
+        for (int i=1; i<nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[index]=nums[i];
+                index++;
+            }
+        }
+
+        return index;
+
+    }
+}
